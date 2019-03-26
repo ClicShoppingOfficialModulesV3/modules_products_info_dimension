891e3763e135b044edd511cc5a2a744efd646405
ClicShopping - e-commerce Solution <39338579+ClicShopping@users.noreply.github.com>
2019-03-26 15:49:55 -0400

    fix little error
    Signed-off-by: oitsuki <loic.richard2@gmail.com>

--- sources/template/Default/modules/modules_products_info/content/products_info_dimension.php
+++ sources/template/Default/modules/modules_products_info/content/products_info_dimension.php
@@ -13,9 +13,7 @@ use ClicShopping\OM\CLICSHOPPING;
 ?>
 <div class="<?php echo $text_position; ?> col-md-<?php echo $content_width; ?>">
   <div class="separator"></div>
-  <div style="float: <?php echo MODULE_PRODUCTS_INFO_DIMENSION_POSITION; ?>;">
-    <div class="modulesProductsInfoDimension">
-      <span class="modulesProductsInfoDimension"><h3><?php echo CLICSHOPPING::getDef('text_products_dimension') . ' ' . $products_dimension; ?></h3></span>
-    </div>
+  <div class="modulesProductsInfoDimension">
+    <span class="modulesProductsInfoDimension"><h3><?php echo CLICSHOPPING::getDef('text_products_dimension') . ' ' . $products_dimension; ?></h3></span>
   </div>
 </div>
